e8ee77e4f9709784bbb93af0d9a0d48d2f0f88c0
Amanyadav30 <amankyadav111@gmail.com>
2024-05-05 11:21:43 +0530

    PackageAdded

---
+++ GradeCalculator.java
@@ -0,0 +1,8 @@
+package code_soft.aman.grade_calculator;
+import java.util.*;
+    class GradeCalculator{
+        public static void main(String []args){
+
+        }
+
+    }
--- Random.java
+++ Random.java
@@ -1,3 +1,5 @@
+package code_soft.aman.random_game;
+
 class Random{
     public static void main(String[] args) {
         System.out.println("Aman yadav");
